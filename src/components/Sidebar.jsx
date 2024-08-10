a82a73da80f41144636c6a8c2525615f4757cb2c
Alejo Kim Uy <alejokimuy@gmail.com>
2024-08-10 14:40:10 -0600

    Working desktop site

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,31 +2,54 @@ import svgLinkedin from '../assets/linkedin.svg';
 import svgGithub from '../assets/github.svg';
 import svgCross from '../assets/cross.svg';
 import profilePicture from '../assets/icon.jpeg';
+import gifSpyro from '../assets/spyro_running.gif';
+import { useState } from 'react';
 
 export default function Sidebar({ showSidebar, setShowSidebar }) {
+	let [count, setCount] = useState(0);
+	let [displaySpyro, setDisplaySpyro] = useState(false);
 	function handleClickCross() {
 		setShowSidebar(false);
+	}
+
+	function handleClickProfilePicture() {
+		if (count > 1) {
+			setCount(0);
+			setDisplaySpyro(true);
+			setTimeout(() => {
+				setDisplaySpyro(false);
+			}, 3000);
+		} else {
+			setCount(count + 1);
+		}
 	}
 
 	return (
 		<div
 			className={
-				'absolute md:fixed flex flex-col justify-evenly w-full h-full z-bg-blue-dark z-10 sidebar ' +
+				'absolute sm:fixed flex flex-col justify-evenly w-full sm:w-2/12 h-full z-bg-blue-dark z-10 sm:translate-x-0 sidebar ' +
 				(showSidebar ? 'sidebar-active' : '')
 			}>
-			<div onClick={handleClickCross} className="ml-4 mt-4 mb-4 shrink-1">
+			<div
+				onClick={handleClickCross}
+				className="sm:hidden ml-4 mt-4 mb-4 shrink-1">
 				<img className="w-10" src={svgCross} alt="Cross" />
 			</div>
-			<div className="grow">
+			<div onClick={handleClickProfilePicture} className="grow sm:grow-0">
 				<div className="flex justify-center">
 					<img
-						className="w-6/12"
+						className={'w-6/12 ' + (displaySpyro ? 'hidden' : '')}
 						src={profilePicture}
+						alt="Profile Picture"
+					/>
+					<img
+						className={'w-6/12 ' + (displaySpyro ? '' : 'hidden')}
+						src={gifSpyro}
 						alt="Profile Picture"
 					/>
 				</div>
 			</div>
-			<nav className="grow text-white flex flex-col items-center">
+			<nav className="grow sm:grow-0 text-white flex flex-col items-center">
 				<ul>
 					<a href="">
 						<li className="py-2">Home</li>
@@ -37,7 +60,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
 					</a>
 				</ul>
 			</nav>
-			<div className="grow flex justify-center">
+			<div className="grow sm:grow-0 flex justify-center">
 				<div className="w-full flex justify-center gap-4">
 					<a href="https://www.linkedin.com/in/devaku/">
 						<img
