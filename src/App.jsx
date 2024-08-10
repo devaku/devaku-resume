bccd963705a965f11f58991fd388142bbd7b6ab7
Alejo Kim Uy <alejokimuy@gmail.com>
2024-08-10 17:11:17 -0600

    Converted sidebar from fixed to sticky

--- src/App.jsx
+++ src/App.jsx
@@ -15,201 +15,208 @@ function App() {
 					<Sidebar
 						showSidebar={showSidebar}
 						setShowSidebar={setShowSidebar}></Sidebar>
-					<main className={'w-11/12 md:w-6/12 mx-auto text-white '}>
-						<Header setShowSidebar={setShowSidebar}></Header>
-						<section className="my-4">
-							<h3 className="text-3xl roboto-serif-700">
-								Profile
-							</h3>
-							<p className="text-justify">
-								A skilled professional, bringing a strong work
-								ethic, web development expertise, and simple yet
-								creative solutions to technical problems. Eager
-								to join the workforce, I am ready to hit the
-								ground running and apply myself to the job to
-								the best of my ability. My goal is to put my
-								knowledge, skills and understanding to good use
-								and provide remarkable service to the
-								organization.
-							</p>
-						</section>
-
-						<section className="my-4 overflow-hidden">
-							<div>
+					<div className="flex flex-col">
+						<main
+							className={'w-11/12 md:w-9/12 mx-auto text-white '}>
+							<Header setShowSidebar={setShowSidebar}></Header>
+							<section className="my-4">
 								<h3 className="text-3xl roboto-serif-700">
-									Experience
+									Profile
 								</h3>
-							</div>
-							<div>
-								<div className="flex">
-									<div className="mr-2">
-										<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
-										<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
-									</div>
-									<div className="">
-										<div className="font-bold">
-											<span>Web Developer</span>
-											<span>TinkrTech Inc</span>
+								<p className="text-justify">
+									A skilled professional, bringing a strong
+									work ethic, web development expertise, and
+									simple yet creative solutions to technical
+									problems. Eager to join the workforce, I am
+									ready to hit the ground running and apply
+									myself to the job to the best of my ability.
+									My goal is to put my knowledge, skills and
+									understanding to good use and provide
+									remarkable service to the organization.
+								</p>
+							</section>
+
+							<section className="my-4 overflow-hidden">
+								<div>
+									<h3 className="text-3xl roboto-serif-700">
+										Experience
+									</h3>
+								</div>
+								<div>
+									<div className="flex">
+										<div className="mr-2">
+											<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
+											<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
 										</div>
 										<div className="">
-											<div>April 2022 - April 2024</div>
-											<div>
-												Dasmariñas City, Cavite,
-												Philippines
+											<div className="font-bold">
+												<span>Web Developer</span>
+												<span>TinkrTech Inc</span>
+											</div>
+											<div className="">
+												<div>
+													April 2022 - April 2024
+												</div>
+												<div>
+													Dasmariñas City, Cavite,
+													Philippines
+												</div>
+											</div>
+
+											<div className="text-justify">
+												<p>
+													Developed microservices to
+													break down a currently
+													existing monolithic
+													software.
+												</p>
+												<p>
+													Led the creation of various
+													REST APIs that interfaced
+													with different applications
+													in the company.
+												</p>
+												<p>
+													Assisted in the continued
+													maintenance of the software
+													of the company.
+												</p>
 											</div>
 										</div>
-
-										<div className="text-justify">
-											<p>
-												Developed microservices to break
-												down a currently existing
-												monolithic software.
-											</p>
-											<p>
-												Led the creation of various REST
-												APIs that interfaced with
-												different applications in the
-												company.
-											</p>
-											<p>
-												Assisted in the continued
-												maintenance of the software of
-												the company.
-											</p>
-										</div>
 									</div>
-								</div>
 
-								<div className="flex">
-									<div className="mr-2">
-										<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
-									</div>
-									<div className="">
-										<div className="font-bold">
-											<span>
-												Information Technology
-												Programmer
-											</span>
-											<span>HTech Corporation</span>
+									<div className="flex">
+										<div className="mr-2">
+											<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
 										</div>
 										<div className="">
-											<div>April 2016 - May 2022</div>
-											<div>
-												Muntinlupa, Alabang, Philippines
+											<div className="font-bold">
+												<span>
+													Information Technology
+													Programmer
+												</span>
+												<span>HTech Corporation</span>
 											</div>
-										</div>
-										<div className="text-justify">
-											<p>
-												Developed a Rule-Based Chatbot
-												Application: GetBotty.
-											</p>
-											<p>
-												Integrated different
-												payment-gateway services to the
-												GetBotty project: Stripe,
-												PayMaya, GrabPay
-											</p>
-											<p>
-												Created various technical
-												applications for internal use
-												within the company.
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</section>
-
-						<section>
-							<div>
-								<h3 className="text-3xl roboto-serif-700">
-									Skills
-								</h3>
-							</div>
-
-							<div className="grid grid-cols-3 gap-1 text-center">
-								<div>HTML</div>
-								<div>CSS</div>
-								<div>JavaScript</div>
-								<div>React</div>
-								<div>Express</div>
-								<div>NodeJS</div>
-								<div>SQL</div>
-								<div>Python</div>
-							</div>
-						</section>
-
-						<section className="my-4 overflow-hidden">
-							<div>
-								<h3 className="text-3xl roboto-serif-700">
-									Education
-								</h3>
-							</div>
-							<div>
-								<div className="flex">
-									<div className="mr-2">
-										<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
-										<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
-									</div>
-									<div className="">
-										<div className="font-bold">
-											Diploma in Software Development
-										</div>
-										<div>
-											<div>
-												Expected Date: December 2025
+											<div className="">
+												<div>April 2016 - May 2022</div>
+												<div>
+													Muntinlupa, Alabang,
+													Philippines
+												</div>
 											</div>
-											<div>
-												Southern Alberta Institute of
-												Technology
+											<div className="text-justify">
+												<p>
+													Developed a Rule-Based
+													Chatbot Application:
+													GetBotty.
+												</p>
+												<p>
+													Integrated different
+													payment-gateway services to
+													the GetBotty project:
+													Stripe, PayMaya, GrabPay
+												</p>
+												<p>
+													Created various technical
+													applications for internal
+													use within the company.
+												</p>
 											</div>
 										</div>
 									</div>
 								</div>
+							</section>
 
-								<div className="flex">
-									<div className="mr-2">
-										<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
-										<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
-									</div>
-									<div className="">
-										<div className="font-bold">
-											Master in Information Technology
+							<section>
+								<div>
+									<h3 className="text-3xl roboto-serif-700">
+										Skills
+									</h3>
+								</div>
+
+								<div className="grid grid-cols-3 gap-1 text-center">
+									<div>HTML</div>
+									<div>CSS</div>
+									<div>JavaScript</div>
+									<div>React</div>
+									<div>Express</div>
+									<div>NodeJS</div>
+									<div>SQL</div>
+									<div>Python</div>
+								</div>
+							</section>
+
+							<section className="my-4 overflow-hidden">
+								<div>
+									<h3 className="text-3xl roboto-serif-700">
+										Education
+									</h3>
+								</div>
+								<div>
+									<div className="flex">
+										<div className="mr-2">
+											<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
+											<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
 										</div>
 										<div className="">
-											<div>2017 - 2021</div>
+											<div className="font-bold">
+												Diploma in Software Development
+											</div>
 											<div>
-												De La Salle
-												University–Dasmariñas
+												<div>
+													Expected Date: December 2025
+												</div>
+												<div>
+													Southern Alberta Institute
+													of Technology
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
 
-								<div className="flex">
-									<div className="mr-2">
-										<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
-									</div>
-									<div className="">
-										<div className="font-bold">
-											Bachelor of Science in Computer
-											Science
+									<div className="flex">
+										<div className="mr-2">
+											<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
+											<span className="block w-1 h-full translate-x-[6px] z-bg-yellow"></span>
 										</div>
 										<div className="">
-											<div>2011 - 2014</div>
-											<div>
-												De La Salle
-												University–Dasmariñas
+											<div className="font-bold">
+												Master in Information Technology
+											</div>
+											<div className="">
+												<div>2017 - 2021</div>
+												<div>
+													De La Salle
+													University–Dasmariñas
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div className="flex">
+										<div className="mr-2">
+											<span className="block w-4 h-4 rounded-full mt-1 z-bg-yellow"></span>
+										</div>
+										<div className="">
+											<div className="font-bold">
+												Bachelor of Science in Computer
+												Science
+											</div>
+											<div className="">
+												<div>2011 - 2014</div>
+												<div>
+													De La Salle
+													University–Dasmariñas
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</section>
-					</main>
+							</section>
+						</main>
+						<Footer></Footer>
+					</div>
 				</div>
-
-				<Footer></Footer>
 			</div>
 		</>
 	);
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,56 +27,67 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
 	return (
 		<div
 			className={
-				'absolute sm:fixed flex flex-col justify-evenly w-full sm:w-2/12 h-full z-bg-blue-dark z-10 sm:translate-x-0 sidebar ' +
+				'absolute sm:sticky w-full sm:w-5/12 z-10 h-screen top-0 sm:translate-x-0 sidebar ' +
 				(showSidebar ? 'sidebar-active' : '')
 			}>
 			<div
-				onClick={handleClickCross}
-				className="sm:hidden ml-4 mt-4 mb-4 shrink-1">
-				<img className="w-10" src={svgCross} alt="Cross" />
-			</div>
-			<div onClick={handleClickProfilePicture} className="grow sm:grow-0">
-				<div className="flex justify-center">
-					<img
-						className={'w-6/12 ' + (displaySpyro ? 'hidden' : '')}
-						src={profilePicture}
-						alt="Profile Picture"
-					/>
-					<img
-						className={'w-6/12 ' + (displaySpyro ? '' : 'hidden')}
-						src={gifSpyro}
-						alt="Profile Picture"
-					/>
+				className={
+					'flex flex-col justify-evenly w-full h-full z-bg-blue-dark'
+				}>
+				<div
+					onClick={handleClickCross}
+					className="sm:hidden ml-4 mt-4 mb-4 shrink-1">
+					<img className="w-10" src={svgCross} alt="Cross" />
 				</div>
-			</div>
-			<nav className="grow sm:grow-0 text-white flex flex-col items-center">
-				<ul>
-					<a href="">
-						<li className="py-2">Home</li>
-					</a>
-
-					<a href="mailto:alejokimwork@gmail.com">
-						<li className="py-2">Contact Me</li>
-					</a>
-				</ul>
-			</nav>
-			<div className="grow sm:grow-0 flex justify-center">
-				<div className="w-full flex justify-center gap-4">
-					<a href="https://www.linkedin.com/in/devaku/">
+				<div
+					onClick={handleClickProfilePicture}
+					className="grow sm:grow-0">
+					<div className="flex justify-center">
 						<img
-							className="inline-block invert"
-							src={svgLinkedin}
-							alt="LinkedIn"
+							className={
+								'w-6/12 ' + (displaySpyro ? 'hidden' : '')
+							}
+							src={profilePicture}
+							alt="Profile Picture"
 						/>
-					</a>
-
-					<a href="https://github.com/devaku?tab=repositories">
 						<img
-							className="inline-block invert"
-							src={svgGithub}
-							alt="Github"
+							className={
+								'w-6/12 ' + (displaySpyro ? '' : 'hidden')
+							}
+							src={gifSpyro}
+							alt="Profile Picture"
 						/>
-					</a>
+					</div>
+				</div>
+				<nav className="grow sm:grow-0 text-white flex flex-col items-center">
+					<ul>
+						<a href="">
+							<li className="py-2">Home</li>
+						</a>
+
+						<a href="mailto:alejokimwork@gmail.com">
+							<li className="py-2">Contact Me</li>
+						</a>
+					</ul>
+				</nav>
+				<div className="grow sm:grow-0 flex justify-center">
+					<div className="w-full flex justify-center gap-4">
+						<a href="https://www.linkedin.com/in/devaku/">
+							<img
+								className="inline-block invert"
+								src={svgLinkedin}
+								alt="LinkedIn"
+							/>
+						</a>
+
+						<a href="https://github.com/devaku?tab=repositories">
+							<img
+								className="inline-block invert"
+								src={svgGithub}
+								alt="Github"
+							/>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
